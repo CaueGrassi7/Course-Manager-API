96a53ba69f7aa37b830c753e3048d5f22ef7d157
CaueGrassi <128317143+CaueGrassi7@users.noreply.github.com>
2024-08-22 10:35:44 -0300

    Initial commit

---
+++ src/main/java/com/cauegrassi/coursemanager/coursemanager_api/CoursemanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.cauegrassi.coursemanager.coursemanager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemanagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cauegrassi/coursemanager/coursemanager_api/CoursemanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauegrassi.coursemanager.coursemanager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
